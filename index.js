2e51ef71c8b947016292bc5b287976f9e6da2637
TSephanya Brun <tsbrun@icloud.com>
2021-03-24 15:52:45 -0400

    commit tree 413e650675e181ea95f2c86f7aa24f8b432cf6f3

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = (drivers) => {
+    let firstTwoDrivers = drivers.slice(0, 2)
+    return firstTwoDrivers
+}
+
+const returnLastTwoDrivers = (drivers) => {
+    let lastTwoDrivers = drivers.slice(drivers.length - 2, drivers.length)
+    return lastTwoDrivers
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier
+    }
+}
+
+const fareDoubler = function(fare) {
+    return createFareMultiplier(2)(fare)
+}
+
+// If I didn't return the function returned by createFareMultiplier(), then the result of fareDoubler would be 'undefined'
+
+const fareTripler = function(fare) {
+    return createFareMultiplier(3)(fare)
+}
+
+function selectDifferentDrivers(drivers, func) {
+    return func(drivers)
+}
